28a0a612a970ccb241e464b274f8a00851d3c211
Frank <eggen@emden.net>
2019-09-22 18:36:06 +0200

    Add gts4lvlte device

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter gts4lvwifi,$(TARGET_DEVICE)),)
+ifneq ($(filter gts4lvwifi gts4lvlte,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
